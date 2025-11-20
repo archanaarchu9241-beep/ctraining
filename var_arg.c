891eb06d91ab3923c1c581417f99c8f0b755d0b3
archanaarchu9241-beep <archanaarchu9241@gmail.com>
2025-11-20 12:30:34 +0530

    Add files via upload

---
+++ var_arg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdarg.c>
+void display(int count,...)
+{
+    va_list list;
+    va_start (list,count);
+    for(int i=count; i<=1; i++)
+    {
+        int n=va_arg(list,int);
+        printf("%d",n);
+    }
+    va_end(list);
+}
+ void main()
+ {
+    display=(4,3,5,7,11);
+ }
+
+
